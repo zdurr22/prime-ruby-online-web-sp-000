15cae5062361a15228d756917d51fb627d1e5f17
Zachary Durr <zdurr22@gmail.com>
2020-10-05 18:40:25 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,12 @@
 # Add  code here!
+def prime?(num)
+  counter = 2
+  if num > 1
+    range = (counter..num-1).to_a
+    range.none? do |num_test|
+      num % num_test == 0
+    end
+  else
+    false
+  end
+ end
