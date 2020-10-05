9fc63eb1bb7a971e28d548fa21f66e1bcb484a3b
Zachary Durr <zdurr22@gmail.com>
2020-10-05 18:24:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,6 +1,7 @@
 # Add  code here!
+require 'prime'
 def prime?(num)
-  return false if num <= 1
-   Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
-   true
+  return false if num.prime? == false
+  else  
+   false
  end
